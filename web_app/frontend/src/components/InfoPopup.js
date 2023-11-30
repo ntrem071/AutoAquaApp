97947d5a52485e9b7af4ea930549f188f7a0c387
Natasha Tremblay Daszkiewicz <90850975+ntrem071@users.noreply.github.com>
2023-11-30 01:12:34 -0500

    Update to Info Popup

--- web_app/frontend/src/components/InfoPopup.js
+++ web_app/frontend/src/components/InfoPopup.js
@@ -15,6 +15,17 @@ function InfoPopup(props) {
         }
       }, [props.trigger, props.name, props.type]);
 
+    const handleButtonClick=(e)=>{
+        switch(e.target.id){
+            case 'add-plant':
+                console.log("a");
+                break;
+            case 'remove-plant':
+                console.log("b");
+                break;
+        }
+    }
+    
     function displayPlantInfoBox(name){
 
         var url = 'http://localhost:8000/plants/na/'+name;
@@ -35,9 +46,9 @@ function InfoPopup(props) {
             }
         })
         .then(data => {
-            console.log(data);
 
             var tb = document.getElementById("popup-topbar");
+            var s0 = document.getElementById("popup-section0");
             var s1 = document.getElementById("popup-section1");
             var s3 = document.getElementById("popup-section3");
 
@@ -46,26 +57,37 @@ function InfoPopup(props) {
             tb.appendChild(d1);
 
            var p1 = document.createElement("p");
-           var l1= document.createElement("label");
-           var l2 = document.createElement("label")
-           l1.className = 'element-label';
-           l1.innerHTML = "Daily Light Requirements: ";
-           l2.className = 'info-label';
-           l2.innerHTML = data.daily_light_requirement[0]+"-"+data.daily_light_requirement[1]+" hours\n";
+           var l1= document.createElement("label"); l1.className = 'element-label';   
+           var l2 = document.createElement("label"); l2.className = 'info-label';
+           l1.innerHTML = "Daily Light: ";
+           l2.innerHTML = "  "+data.daily_light_requirement[0]+"-"+data.daily_light_requirement[1]+" hours\n";
            p1.appendChild(l1);p1.appendChild(l2);s1.appendChild(p1);
-          // var d3 = document.createElement("p");
-           //d3.innerHTML = "Ideal PH Range: "+ data.ph_range[0]+"-"+data.ph_range[1];
-           //var d4 = document.createElement("p");
-           //d4.innerHTML = "Ideal EC Range: "+ data.ec_range[0]+"-"+data.ec_range[1];
 
-            //elBtn.appendChild(document.createTextNode(data[i]));
-            
+           var p2 = document.createElement("p");
+           var l3= document.createElement("label");l3.className = 'element-label';
+           var l4 = document.createElement("label"); l4.className = 'info-label';
+           l3.innerHTML = "Ideal PH Range: ";
+           l4.innerHTML = "  "+ data.ph_range[0]+"-"+data.ph_range[1];
+           p2.appendChild(l3);p2.appendChild(l4);s1.appendChild(p2);
 
-            //s1.appendChild(d2);
-            //s1.appendChild(d3);
-            //s1.appendChild(d4);
+           var p3 = document.createElement("p");
+           var l5= document.createElement("label"); l5.className = 'element-label';
+           var l6 = document.createElement("label");l6.className = 'info-label';
+           l5.innerHTML = "Ideal EC Range: ";
+           l6.innerHTML = "  "+ data.ec_range[0]+"-"+data.ec_range[1];
+           p3.appendChild(l5);p3.appendChild(l6);s1.appendChild(p3);
 
-            
+           var b1 = document.createElement("button");
+           var d2 = document.createElement("div"); 
+           b1.id='add-plant'; b1.addEventListener("click", (e)=>handleButtonClick(e)); b1.innerHTML = "Add";
+           var b2 = document.createElement("button");
+           b2.id='remove-plant'; b2.addEventListener("click", (e)=>handleButtonClick(e)); b2.innerHTML = "Remove";
+           s0.appendChild(b1);
+           s0.appendChild(b2);
+
+           var p3 = document.createElement("p"); p3.className='popup-info';
+           p3.innerHTML = data.info;
+           s3.appendChild(p3);
         })
         .catch((err) => {
             setError(err);
@@ -92,6 +114,48 @@ function InfoPopup(props) {
         })
         .then(data => {
             console.log(data);
+
+            var tb = document.getElementById("popup-topbar");
+            var s0 = document.getElementById("popup-section0");
+            var s1 = document.getElementById("popup-section1");
+            var s3 = document.getElementById("popup-section3");
+
+            var d1 = document.createElement("label");
+            d1.innerHTML = data.fish;
+            tb.appendChild(d1);
+
+
+           var p2 = document.createElement("p");
+           var l3= document.createElement("label");l3.className = 'element-label';
+           var l4 = document.createElement("label"); l4.className = 'info-label';
+           l3.innerHTML = "Ideal PH Range: ";
+           l4.innerHTML = "  "+ data.ph_range[0]+"-"+data.ph_range[1];
+           p2.appendChild(l3);p2.appendChild(l4);s1.appendChild(p2);
+
+           var p3 = document.createElement("p");
+           var l5= document.createElement("label"); l5.className = 'element-label';
+           var l6 = document.createElement("label");l6.className = 'info-label';
+           l5.innerHTML = "Ideal Temp Range: ";
+           l6.innerHTML = "  "+ data.temp_range[0]+"-"+data.temp_range[1];
+           p3.appendChild(l5);p3.appendChild(l6);s1.appendChild(p3);
+           
+           var p1 = document.createElement("p");
+           var l1= document.createElement("label"); l1.className = 'element-label';   
+           var l2 = document.createElement("label"); l2.className = 'info-label';
+           l1.innerHTML = "Tank Size: ";
+           l2.innerHTML = "  "+data.tank_size;
+           p1.appendChild(l1);p1.appendChild(l2);s1.appendChild(p1);
+
+           var b1 = document.createElement("button");
+           b1.id='add-plant'; b1.addEventListener("click", (e)=>handleButtonClick(e)); b1.innerHTML = "Add";
+           var b2 = document.createElement("button");
+           b2.id='remove-plant'; b2.addEventListener("click", (e)=>handleButtonClick(e)); b2.innerHTML = "Remove";
+           s0.appendChild(b1);
+           s0.appendChild(b2);
+
+           var p3 = document.createElement("p"); p3.className='popup-info';
+           p3.innerHTML = data.info;
+           s3.appendChild(p3);
             
         })
         .catch((err) => {
@@ -114,8 +178,12 @@ function InfoPopup(props) {
                         {props.children}
                     </div>
                     <div className='popup-info-display'>
+                        
                         <div className='popup-section1' id='popup-section1'></div>
-                        <div className='popup-section2' id='popup-section2'></div>
+                        <div className='popup-section0' id='popup-section0'></div>
+                        <div className='popup-section2' id='popup-section2'>
+                            <label className='popup-temp'>Image Placeholder</label>
+                        </div>
                         <div className='popup-section3' id='popup-section3'></div>
                     </div>
                 </div>
