b5c35d4838ac28925cb3f39331f5b48f61eca197
IMansouri <79425861+IMansouri@users.noreply.github.com>
2024-02-28 02:28:12 -0500

    implemented graph point in server with sensors data

--- hardware/jn-serialCommunication.py
+++ hardware/jn-serialCommunication.py
@@ -3,25 +3,59 @@
 import serial #import PySerial
 import datetime
 import time
+import requests
+import struct
+import json
 
-arduinoMega=serial.Serial(
-    port = '/dev/ttyTHS1', #UART ports 8(Rx) & 10(Tx)
-    baudrate = 9600
-)
+#arduinoMega=serial.Serial(
+ #   port = '/dev/ttyTHS1', #UART ports 8(Rx) & 10(Tx)
+  #  baudrate = 9600
+#)
 
+serverHeader = {
+	#'Authorization': 'Basic' + buf,
+	'Accept': 'application/json',
+	'Content-Type': 'application/json'
+}
+
+userCreds = json.dumps({
+    "email": "lol@pog.ca",
+    "password": "c"
+})
+
+getSID = requests.post('https://ceg4913-server.duckdns.org/users/na/login/system', headers=serverHeader, data=userCreds, timeout=5)
+print(getSID.status_code)
+sessionId = json.loads(getSID.text)["sessionId"]
+print(sessionId)
 
 while True:
-    try:
-	
-        data = arduinoMega.readline().rstrip().split(",")
+	try:
+        #data = arduinoMega.readline().rstrip().split(",")
 	    #[pH,temperature,water level,EC]
-        if data:
-            print(datetime.datetime.now())
-            print(data)
-            print("\n")
-        else:
-            print("No data found") 
-            time.sleep(1)
+		if True:
+			print(datetime.datetime.now())
+			#print(data)
+			# probeData = json.dumps({
+			#     "phPoint": struct.unpack('d', data[0])[0],
+			#     "tempPoint": struct.unpack('d', data[1])[0], 
+			#     "waterPoint": struct.unpack('d', data[2])[0],
+			#     "ecPoint": struct.unpack('d', data[3])[0]
+			# })
 
-    except Exception as e:
-        print(e)
+			#temporary data when arduino not accessible (only for testing the graph datapoints!!!)
+			probeData = json.dumps({
+			"phPoint": 6.8,
+			"tempPoint": 17.4, 
+			"waterPoint": 300.0,
+			"ecPoint": 2.5
+			})
+			resp = requests.put('https://ceg4913-server.duckdns.org/users/'+ sessionId + '/graphs', data=probeData, headers=serverHeader, timeout=5) 
+			print(resp.status_code)
+			print("Collected data successfully!")
+			time.sleep(3)
+		else:
+			print("No data found") 
+			time.sleep(1)
+
+	except Exception as e:
+		print(e)
